07765513c3588676c87dfaf9945d4a7685fb2a93
Justin Shenk <shenkjustin@gmail.com>
2019-05-22 13:16:33 +0200

    Add tests

--- closest_pairs/tests/test_solve.py
+++ closest_pairs/tests/test_solve.py
@@ -1,13 +1,12 @@
 import numpy as np
 import pytest
 
-import closest_points
+import closest_pairs
 
 @pytest.mark.parametrize("n_points,n_dim", [(2,2),(2,3),(3,2),(3,3)])
-import pytest
 def test_solution(n_points, n_dim):
-    X = np.random.random((1000,n=n_dim))
-    pairs, distances = closest_points.solve(X,n=n_points)
+    X = np.random.random((1000,n_dim))
+    pairs, distances = closest_pairs.solve(X,n=n_points)
     assert isinstance(pairs,np.ndarray)
     assert len(pairs) == n_points
     assert isinstance(distances, np.ndarray)
